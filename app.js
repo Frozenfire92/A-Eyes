ec8e07cddabf8bee6b3bc3920cfa16f36a6d00ee
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-03-21 17:02:58 -0300

    Speech2Text :D

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
 /*jshint node:true*/
 //Dependencies
-var express = require('express');
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var watson = require('watson-developer-cloud');
-
-// setup server middleware
-var app = express();
-app.use(app.router);
-app.use(express.errorHandler());
-app.use(express.static(__dirname + '/public')); //setup static public directory
-app.use(express.static(__dirname + '/bower_components'));
-
-// Bluemix app & service info
-var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
-// TODO: Get application information and use it in your app.
-var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
-// TODO: Get service credentials and communicate with bluemix services.
 
 // Setup watson speech->text
 var speech_to_text = watson.speech_to_text({
@@ -30,52 +19,11 @@ var text_to_speech = watson.text_to_speech({
 	version: 'v1'
 });
 
-// Synthesize some speach into text from a GET request
-app.get('/synthesize', function(req, res) {
-	var synthReq = {
-		accept: "audio/ogg; codecs=opus",
-		voice: "VoiceEnUsMichael",
-		text: req.query.text
-	};
-	var transcript = text_to_speech.synthesize(synthReq);
-	transcript.on('response', function(response) {
-    	console.log(response.headers);
-    	if (req.query.download) {
-      		response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
-    	}
-  	});
-  	transcript.pipe(res);
-});
+//Configure server & sockets
+require('./config/express')(app, speech_to_text, text_to_speech);
+require('./config/socket')(io, speech_to_text);
 
-//----- Example Speach->Text from https://www.npmjs.com/package/watson-developer-cloud
-// var params = {
-//   // From file 
-//   audio: request('http://speech-to-text-demo.mybluemix.net/audio/sample1.wav'),
-//   content_type: 'audio/l16; rate=44100'
-// };
-// speech_to_text.recognize(params, function(err, res) {
-//   if (err)
-//     console.log(err);
-//   else
-//     console.log(JSON.stringify(res, null, 2));
-// });
-
-
-//----- Example Text->Speach from https://www.npmjs.com/package/watson-developer-cloud
-// var params = {
-//     text: 'Hello from IBM Watson',
-//     voice: 'VoiceEnUsMichael', // optional voice 
-//     accept: 'audio/wav'
-// };
-// // pipe the synthesized text to a file 
-// text_to_speech.synthesize(params).pipe(fs.createWriteStream('output.wav'));
-
-
-// The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
-var host = (process.env.VCAP_APP_HOST || 'localhost');
-// The port on the DEA for communication with the application:
-var port = (process.env.VCAP_APP_PORT || 3000);
 // Start server
-app.listen(port, host);
-console.log('App started on port ' + port);
-
+var port = (process.env.VCAP_APP_PORT || 3000);
+server.listen(port);
+console.log('Listening at port: ' + port);
---
+++ config/express.js
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2014 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// Module dependencies
+var express    = require('express'),
+  errorhandler = require('errorhandler'),
+  bodyParser   = require('body-parser'),
+  fs           = require('fs');
+
+module.exports = function (app, speechToText, textToSpeech) {
+
+	// Configure Express
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+
+	// Setup static public directory
+	app.use(express.static(__dirname + '/../public'));
+	app.use(express.static(__dirname + '/../bower_components'));
+
+	// Add error handling in dev
+	if (!process.env.VCAP_SERVICES) app.use(errorhandler());
+
+	// Route For text 2 speech
+	app.get('/synthesize', function(req, res) {
+		var synthReq = {
+			accept: "audio/ogg; codecs=opus",
+			voice: "VoiceEnUsMichael",
+			text: req.query.text
+		};
+		var transcript = textToSpeech.synthesize(synthReq);
+		transcript.on('response', function(response) {
+	    	console.log(response.headers);
+	    	if (req.query.download) {
+	      		response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+	    	}
+	  	});
+	  	transcript.pipe(res);
+	});
+
+	// For speech 2 text
+	app.post('/', function(req, res) {
+		var audio;
+
+		if(req.body.url && req.body.url.indexOf('audio/') === 0) {
+			// sample audio
+	    	audio = fs.createReadStream(__dirname + '/../public/' + req.body.url);
+	  	} else { // malformed url
+	    	return res.status(500).json({ error: 'Malformed URL' });
+	  	}
+
+		speechToText.recognize({audio: audio, content_type: 'audio/l16; rate=44100'}, function(err, transcript){
+	    	if (err) return res.status(500).json({ error: err });
+	    	else return res.json(transcript);
+	  	});
+	});
+};
