3c84301321a369c2fea58c8c1b3cbacf5f433c25
Sarah <rainbee2214@gmail.com>
2015-03-22 10:27:27 -0300

    Moving the player around the world is working.

--- public/js/gameController.js
+++ public/js/gameController.js
@@ -4,14 +4,86 @@ var aeye;
 
 var chests = [];
 var npcs = [];
+var items = [];
 
 function InstantiateWorld()
 {
 	world = CreateWorld("Sarah's World", 5);
-	console.log("This is my world ", world);
 
 	SetUpChests();
 	SetUpNPCs();
+	SetUpBackgrounds();
+
+	console.log("This is my world ", world);
+
+	player = CreatePlayer("Sarah");
+	console.log	("This is me ", player);
+}
+
+function MovePlayer(direction)
+{
+	//Change the player.currentLocation to be the location of the space that it is moving to
+	console.log("Moving player ", direction);
+	console.log("Player is currently at location: ", player.currentLocation);
+	player.currentLocation = FindLocationToMoveTo(direction);
+	console.log("Player (after move)  at location: ", player.currentLocation);
+	
+}
+
+function FindLocationToMoveTo(direction)
+{
+	//Take the space from that direction of the current location and return that new space location
+	var locationToMoveTo;
+
+	for (var i = 0; i <  world.gridBlocks.length; i++) 
+	{
+		for (var j = 0; j < world.gridBlocks[i].blockSpaces.length; j++)
+		{
+			var letterIndex = GetCharacterFromIndex(i);
+			if (player.currentLocation.gridBlockLetterIndex == letterIndex && player.currentLocation.blockSpaceIndex == j)
+			{
+				switch (direction)
+				{
+					case "Up":
+					case "North":
+					{ 
+						var globalIndexOfNewSpace = world.gridBlocks[i].blockSpaces[j].northSpace
+						locationToMoveTo = GetLocationFromGlobalIndex(globalIndexOfNewSpace);
+						break;
+					}
+					case "Down":
+					case "South":
+					{
+						var globalIndexOfNewSpace = world.gridBlocks[i].blockSpaces[j].southSpace
+						locationToMoveTo = GetLocationFromGlobalIndex(globalIndexOfNewSpace);
+						break;
+					}
+					case "Right":
+					case "East":
+					{
+						var globalIndexOfNewSpace = world.gridBlocks[i].blockSpaces[j].eastSpace
+						locationToMoveTo = GetLocationFromGlobalIndex(globalIndexOfNewSpace);
+						break;
+					}
+					case "Left":
+					case "West":
+					{
+						var globalIndexOfNewSpace = world.gridBlocks[i].blockSpaces[j].westSpace
+						locationToMoveTo = GetLocationFromGlobalIndex(globalIndexOfNewSpace);
+						break;
+					}
+					default:
+					{
+						console.log("Bad direction");
+						break;
+					}
+				}
+					console.log("Moving to location " , locationToMoveTo);
+			}
+		}
+	}
+
+	return locationToMoveTo;
 }
 
 function PutObjectInBlockSpace(objectToPut, locationToPut)
@@ -23,27 +95,13 @@ function PutObjectInBlockSpace(objectToPut, locationToPut)
 	return objectToPut;
 }
 
-function GetGridBlock(letterIndex)
-{
-
-}
-
 function GetBlockSpaces(gridBlock)
 {
 	return gridBlock.blockSpaces;
 }
 
-function GetTimeOfDay()
-{
-	return world.timeOfDay;
-}
-
 function FindBlockSpace(location)
 {
-	//Find the block space at that location
-	//Find the Grid block at the letterIndexLocation
-	//Find the block space within that grid block
-
 	var gridBlockIndex = GetIndexFromCharacter(location.gridBlockLetterIndex);
 	return world.gridBlocks[gridBlockIndex].blockSpaces[location.blockSpaceIndex];
 }
@@ -53,22 +111,29 @@ function SetUpChests()
 	//Create all Chests
 	//Set locations of all chests
 
-	var chest1 = PutObjectInBlockSpace(CreateChest("Chest1"), CreateLocation("A", 4));
+	var item1 = CreateItem("Umbrella", "Maybe it will help keep you dry, maybe not!");
+	var chest1 = PutObjectInBlockSpace(CreateChest("Chest1",item1), CreateLocation("A", 4));
 	chests.push(chest1);
 
-	var chest2 = PutObjectInBlockSpace(CreateChest("Chest2"), CreateLocation("D", 0));
+	var item2 = CreateItem("GolddenApple", "This apple will allow all the knowledge to grow inside you!");
+	var chest2 = PutObjectInBlockSpace(CreateChest("Chest2", item2), CreateLocation("D", 0));
 	chests.push(chest2);
 		
-	var chest3 = PutObjectInBlockSpace(CreateChest("Chest3"), CreateLocation("G", 7));
+	var item3 = CreateItem("RainbowLollipop", "Eating this will unlock your wildest dreams!");
+	var chest3 = PutObjectInBlockSpace(CreateChest("Chest3", item3), CreateLocation("G", 7));
 	chests.push(chest3);
 	
-	var chest4 = PutObjectInBlockSpace(CreateChest("Chest4"), CreateLocation("M", 4));
+	var item4 = CreateItem("TalkingGoldfish", "To grant unlimited wishes!");
+
+	var chest4 = PutObjectInBlockSpace(CreateChest("Chest4", item4), CreateLocation("M", 4));
 	chests.push(chest4);
 
-	var chest5 = PutObjectInBlockSpace(CreateChest("Chest5"), CreateLocation("Q", 2));
+	var item5 = CreateItem("YoYo", "Everybody just wants to have fun.");
+	var chest5 = PutObjectInBlockSpace(CreateChest("Chest5", item5), CreateLocation("Q", 2));
 	chests.push(chest5);
 
-	var chest6 = PutObjectInBlockSpace(CreateChest("Chest6"), CreateLocation("X", 5));
+	var item6 = CreateItem("SuperMagnets", "These will help you take over the world!");
+	var chest6 = PutObjectInBlockSpace(CreateChest("Chest6", item6), CreateLocation("X", 5));
 	chests.push(chest6);
 }
 
@@ -100,4 +165,56 @@ function SetUpNPCs()
 
 	var npc8 = PutObjectInBlockSpace(CreateNPC("NPC8"), CreateLocation("Y", 4));
 	npcs.push(npc8);
+}
+
+function SetUpBackgrounds()
+{
+	world.gridBlocks[0].backgroundClass = "Home";
+	world.gridBlocks[1].backgroundClass = "TownSquare";
+	world.gridBlocks[2].backgroundClass = "TownSquare";
+	world.gridBlocks[3].backgroundClass = "TownSquare";
+	world.gridBlocks[4].backgroundClass = "Gamehouse";
+	world.gridBlocks[5].backgroundClass = "HouseGroup1";
+	world.gridBlocks[6].backgroundClass = "TownSquare";
+	world.gridBlocks[7].backgroundClass = "TownSquare";
+	world.gridBlocks[8].backgroundClass = "TownSquare";
+	world.gridBlocks[9].backgroundClass = "HouseGroup2";
+	world.gridBlocks[10].backgroundClass = "HouseGroup1";
+	world.gridBlocks[11].backgroundClass = "TownSquare";
+	world.gridBlocks[12].backgroundClass = "TownSquare";
+	world.gridBlocks[13].backgroundClass = "TownSquare";
+	world.gridBlocks[14].backgroundClass = "HouseGroup2";
+	world.gridBlocks[15].backgroundClass = "HouseGroup1";
+	world.gridBlocks[16].backgroundClass = "TownSquare";
+	world.gridBlocks[17].backgroundClass = "TownSquare";
+	world.gridBlocks[18].backgroundClass = "TownSquare";
+	world.gridBlocks[19].backgroundClass = "HouseGroup2";
+	world.gridBlocks[20].backgroundClass = "HouseGroup1";
+	world.gridBlocks[21].backgroundClass = "TownSquare";
+	world.gridBlocks[22].backgroundClass = "TownSquare";
+	world.gridBlocks[23].backgroundClass = "TownSquare";
+	world.gridBlocks[24].backgroundClass = "HouseGroup2";
+}
+
+function GetBlockSpaceFromGlobalIndex(globalIndex)
+{
+	for (var i = 0; i <  world.gridBlocks.length; i++) 
+	{
+		for (var j = 0; j < world.gridBlocks[i].blockSpaces.length; j++)
+		{
+			if (world.gridBlocks[i].blockSpaces[j].globalIndex == globalIndex) return world.gridBlocks[i].blockSpaces[j];
+		}
+	}
+}
+
+function GetLocationFromGlobalIndex(globalIndex)
+{
+	console.log("Looking for location at global index: ", globalIndex);
+	for (var i = 0; i <  world.gridBlocks.length; i++) 
+	{
+		for (var j = 0; j < world.gridBlocks[i].blockSpaces.length; j++)
+		{
+			if (world.gridBlocks[i].blockSpaces[j].globalIndex == globalIndex) return CreateLocation(GetCharacterFromIndex(i), j);
+		}
+	}
 }
--- public/js/helper.js
+++ public/js/helper.js
@@ -36,3 +36,35 @@ function GetIndexFromCharacter(character)
 	}
 }
 
+function GetCharacterFromIndex(index)
+{
+	switch (index)
+	{
+		case 0: return  "A";
+		case 1: return  "B";
+		case 2: return  "C";
+		case 3: return  "D";
+		case 4: return  "E";
+		case 5: return  "F";
+		case 6: return  "G";
+		case 7: return  "H";
+		case 8: return  "I";
+		case 9: return  "J";
+		case 10: return "K";
+		case 11: return "L";
+		case 12: return "M";
+		case 13: return "N";
+		case 14: return "O";
+		case 15: return "P";
+		case 16: return "Q";
+		case 17: return "R";
+		case 18: return "S";
+		case 19: return "T";
+		case 20: return "U";
+		case 22: return "V";
+		case 23: return "W";
+		case 24: return "X";
+		case 25: return "Y";
+	}
+}
+
