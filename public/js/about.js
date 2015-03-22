7e28f24aed0c12bed12875ea392fd26602751f18
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-03-22 13:36:22 -0300

    About page done

---
+++ public/js/about.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	var description = "About A-Eyes. An Accessible Text Based Adventure Game. Built using IBM Bluemix and the power of Watson's Speech to Text and Text to Speech, A-Eyes invents a new genre of games where accessibility is key. Story. In a world where humans have lost their eyesight, they have created robots called A-Eyes to help them navigate and interact with the world. This is a day in the life of an ordinary human in this world. Instructions. Press space to dictate commands to your A-Eye. Press space again to end the command. Explore the world. Find the 6 hidden items.";
+	textToSpeech(description);
+});
+
